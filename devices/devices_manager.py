252ff1efa75225f6928f7c5ea4f6b616ecd5ffa2
pyngon <pyng1101@gmail.com>
2020-04-03 12:14:51 -0400

    add http server for device monitor

--- devices/devices_manager.py
+++ devices/devices_manager.py
@@ -1,4 +1,7 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
 import json
+import threading
 from device import Device
 from devices_status_db import DevicesStatusDB
 
@@ -7,7 +10,8 @@ Main components for managing devices. Called DevicesManager.start() in initializ
 """
 class DevicesManager:
     alldevs = None
-    
+    monitor_thread = None
+
     @staticmethod
     def start(status_callback = None):
         # The devices.json serve as a database of available device now.
@@ -15,9 +19,16 @@ class DevicesManager:
             DevicesManager.alldevs = json.load(f)
             assert DevicesManager.alldevs is not None
         
-        DevicesMonitor.set_callback(status_callback)
+        # Create a thread to bring up an HTTP server for devices status monitor
+        monitor_thread = DevicesMonitor.start(status_callback)
         return
     
+    @staticmethod
+    def wait():
+        if DevicesManager.monitor_thread is not None:
+            DevicesManager.monitor_thread.join()
+        return
+
     @staticmethod
     def find_devices(devname):
         """
@@ -27,8 +38,6 @@ class DevicesManager:
         """
         if not devname in DevicesManager.alldevs:
             return None
-        
-        # TODO: create a thread to bring up an HTTP server for devices status monitor
 
         dev_obj = DevicesManager.alldevs[devname]
         return Device(dev_obj['id'], dev_obj['name'], dev_obj['type'], dev_obj['location'])
@@ -39,10 +48,29 @@ Simulate the interface that receive updated status from devices.
 """
 class DevicesMonitor:
     callback = None
+
+    class DeviceThread(threading.Thread):
+        def run(self):
+            server_address = ("localhost", 8080)
+            handler_class = MyHandler
+            server_class = HTTPServer
+
+            httpd = HTTPServer(server_address, handler_class)
+            print("server running...")
+
+            try: 
+                httpd.serve_forever()
+            except KeyboardInterrupt: pass
+
+            httpd.server_close()
+
     
     @staticmethod
-    def set_callback(cb):
+    def start(cb):
         DevicesMonitor.callback = cb
+        dev_thread = DevicesMonitor.DeviceThread()
+        dev_thread.start()
+        return dev_thread
 
     @staticmethod
     def update_status(devname, status, value):
@@ -55,3 +83,36 @@ class DevicesMonitor:
         db.set_status(devname, status, value)
         if DevicesMonitor.callback is not None:
             DevicesMonitor.callback(devname, status, value)
+    
+
+class MyHandler(BaseHTTPRequestHandler):
+    def _set_response(self, code):
+        self.send_response(code)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_GET(self):
+        # example: this is how you get path and command
+        print(self.path)
+        print(self.command)
+
+        q_part = parse_qs(urlparse(self.path).query)
+
+        if 'device' in q_part and 'status_name' in q_part \
+            and 'status_value' in q_part:
+            device = q_part['device'][0]
+            status_name = q_part['status_name'][0]
+            status_value = q_part['status_value'][0]
+
+            print('device=%s status_name=%s status_value=%s' % (device, status_name, status_value))
+            DevicesMonitor.update_status(device, status_name, status_value)
+            self._set_response(200)
+            return
+
+        self._set_response(400)
+
+    def do_POST(self):
+        self._set_response(405)
+
+    def do_DELETE(self):
+        self._set_response(405)
---
+++ devices/test.py
@@ -0,0 +1,8 @@
+from devices_manager import DevicesManager
+
+def device_changed(dev, status, value):
+    print('Received status update of ' + dev + '.' + status + '=' + value)
+
+if __name__ == "__main__" :
+    DevicesManager.start(device_changed)
+    DevicesManager.wait()
